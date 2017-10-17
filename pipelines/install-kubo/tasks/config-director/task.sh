d68465e9f61bdb40b71ba81af46565b82b839044
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-16 20:26:14 -0400

    Continued building out director configuration

--- pipelines/install-kubo/tasks/config-director/task.sh
+++ pipelines/install-kubo/tasks/config-director/task.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
 set -eu
+
+director_var=kubo-environment/director.yml
+director_secrets=kubo-environment/director-secrets.yml
+
+cat <<VARIABLES > ${director_var}
+vcenter_ip: ${VCENTER_HOST}
+vcenter_user: ${VCENTER_USER}
+vcenter_dc: ${VCENTER_DATACENTER}
+vcenter_vms: ${KUBO_VM_FOLDER}
+vcenter_templates: ${KUBO_TEMPLATES}
+vcenter_disks: ${KUBO_DISK_PATH}
+vcenter_ds: ${KUBO_EPHEMERAL_STORE}
+vcenter_cluster: ${VCENTER_CLUSTER}
+vcenter_rp: ${VCENTER_RESOURCE_POOL}
+
+internal_ip: ${DIRECTOR_IP}
+deployments_network: ${NETWORK_NAME}
+internal_cidr: ${NETWORK_CIDR}
+internal_gw: ${GATEWAY}
+director_name: ${KUBO_DIRECTOR_NAME}
+dns_recursor_ip: ${DNS}
+kubo_release_url: https://storage.googleapis.com/kubo-public/kubo-release-latest.tgz
+
+network_name: ${NETWORK_NAME}
+reserved_ips: ${EXCLUDED_RANGE}
+
+iaas: vsphere
+VARIABLES
+
+cat <<SECRETS > ${director_secrets}
+vcenter_password: ${VCENTER_PASSWORD}
+SECRETS
