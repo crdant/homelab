acdf0cd862a13f37d561cc8140be3b3322940bed
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-18 15:20:17 -0700

    Corrected vars file variable when adding info

--- pipelines/install-kubo/tasks/config-router/task.sh
+++ pipelines/install-kubo/tasks/config-router/task.sh
@@ -3,11 +3,11 @@
 set -eu
 
 # This is probably not the best way to do this, but it feels a bit better than hard coding and/or abusing ops files
-director_var=environment-state/director.yml
+director_vars=environment-state/director.yml
 director_secrets=environment-state/director-secrets.yml
 
 # add routing configuration
-cat <<ROUTING >> ${director_yml}
+cat <<ROUTING >> ${director_vars}
 routing_mode: ${ROUTING_MODE}
 kubernetes_master_host: ${KUBERNETES_MASTER_HOST}
 kubernetes_master_port: ${KUBERNETES_MASTER_PORT}
