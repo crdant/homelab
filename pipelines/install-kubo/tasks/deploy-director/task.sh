0c977aca4aeac3d59d034813e912b62b1e38cbac
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-23 21:23:15 -0400

    Change to correct directory

--- pipelines/install-kubo/tasks/deploy-director/task.sh
+++ pipelines/install-kubo/tasks/deploy-director/task.sh
@@ -7,7 +7,7 @@ director_secrets=director-config/director-secrets.yml
 bosh_creds=director-config/creds.yml
 bosh_state=director-config/state.json
 
-cd kubo-odb-deployment/kubo-deployment
+cd kubo-odb-deployment
 ./bin/deploy_bosh director-config
 
 cp ${director_vars} director-state
