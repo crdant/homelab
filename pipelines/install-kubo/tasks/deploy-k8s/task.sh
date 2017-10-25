07a290ddbe9a814c3b235bf1e5f5c0710408b052
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-25 19:28:29 -0400

    Added missing character...will work much better now

--- pipelines/install-kubo/tasks/deploy-k8s/task.sh
+++ pipelines/install-kubo/tasks/deploy-k8s/task.sh
@@ -4,7 +4,7 @@ set -eu
 
 # workaround braindead naming in script
 cp /usr/bin/bosh2 /usr/local/bin/bosh-cli
-bosh-cli alias-env environment-state -e (bosh-cli int environment-state/director.yml --path /internal_ip) --ca-cert <(bosh int environment-state/creds.yml --path /director_ssl/ca)
+bosh-cli alias-env environment-state -e $(bosh-cli int environment-state/director.yml --path /internal_ip) --ca-cert <(bosh int environment-state/creds.yml --path /director_ssl/ca)
 
 pushd environment-state
 config="$(pwd)"
