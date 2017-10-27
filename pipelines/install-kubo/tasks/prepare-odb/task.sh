f2bae6eeb2046453ab23567ea5e0e131fc9f648a
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-27 13:38:48 -0400

    Handle stemcell

--- pipelines/install-kubo/tasks/prepare-odb/task.sh
+++ pipelines/install-kubo/tasks/prepare-odb/task.sh
@@ -6,3 +6,4 @@ set -eu
 cp odb-sdk/*.tgz odb-releases/on-demand-service-broker.tgz
 cp kubo-service-adapter/*.tgz odb-releases/kubo-service-adapter-release.tgz
 cp kubo-release/*.tgz odb-releases/kubo-release.tgz
+cp stemcell/*.tgz odb-releases
