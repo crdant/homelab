518a194b6a65e204dfdb29b5a4e4588c42496155
Chuck D'Antonio <c_dantonio@acm.org>
2017-10-27 21:03:51 -0400

    Caught up variable definitions with change to how I get the director name

--- pipelines/install-kubo/tasks/config-odb/task.sh
+++ pipelines/install-kubo/tasks/config-odb/task.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-secret_root="${DIRECTOR_NAME}/${DEPLOYMENT_NAME}"
+director_name=$(bosh2 interpolate "${kubo_config_path}/director.yml" --path="/director_name")
+secret_root="${director_name}/${DEPLOYMENT_NAME}"
 director_state_path="environment-state"
 
 uaa_ca=$(bosh2 int "${director_state_path}/creds.yml" --path="/uaa_ssl/ca")
@@ -10,8 +11,6 @@ credhub_ca=$(bosh2 int "${director_state_path}/creds.yml" --path="/credhub_tls/c
 credhub_password=$(bosh2 int "${director_state_path}/creds.yml" --path="/credhub_cli_password")
 director_ip=$(bosh2 int "${director_state_path}/director.yml" --path="/internal_ip")
 credhub login -u credhub-cli -p ${credhub_password} -s "https://${director_ip}:8844" --ca-cert "${uaa_ca}"  --ca-cert "${credhub_ca}"
-
-director_name=$(bosh2 interpolate "${kubo_config_path}/director.yml" --path="/director_name")
 
 credhub set --name="${secret_root}/cf_sys_domain" --type="value" --value="${PCF_SYSTEM_DOMAIN}"
 credhub set --name="${secret_root}/cf_username" --type="value" --value="${PCF_USERNAME}"
