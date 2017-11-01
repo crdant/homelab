a59d75bfc31255b7efa6db1751f1b622be26ea54
Chuck D'Antonio <c_dantonio@acm.org>
2017-11-01 09:13:22 -0400

    Fixed BOSH command name other places

--- pipelines/install-kubo/tasks/wipe-env/task.sh
+++ pipelines/install-kubo/tasks/wipe-env/task.sh
@@ -3,7 +3,7 @@
 set -eu
 
 # workaround braindead naming in script
-bosh2 alias-env kubo -e $(bosh-cli int environment-state/director.yml --path /internal_ip) --ca-cert <(bosh-cli int environment-state/creds.yml --path /director_ssl/ca)
+bosh2 alias-env kubo -e $(bosh2 int environment-state/director.yml --path /internal_ip) --ca-cert <(bosh2 int environment-state/creds.yml --path /director_ssl/ca)
 
 pushd environment-state
 config="$(pwd)"
@@ -14,5 +14,5 @@ export BOSH_CLIENT_SECRET=$(bosh int environment-state/creds.yml --path /admin_p
 export BOSH_ENVIRONMENT=kubo
 export BOSH_DEPLOYMENT=${KUBO_SERVICE_NAME}
 
-bosh delete-deployment
-bosh clean-up
+bosh2 delete-deployment
+bosh2 clean-up
