dbd6d439a61158e607a59d5bf24b38c1d51d6ee9
Chuck D'Antonio <c_dantonio@acm.org>
2017-11-01 20:56:00 -0400

    Got the username and password

--- pipelines/install-pcf/tasks/config-tcp/task.sh
+++ pipelines/install-pcf/tasks/config-tcp/task.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-local username=
-local password=
+username=$(om -t http://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -u ${OPSMAN_USERNAME} -p ${OPSMAN_PASSWORD} credentials --product-name cf --credential-reference .uaa.admin_credentials --credential-field identity)
+password=$(om -t http://${OPSMAN_DOMAIN_OR_IP_ADDRESS} -u ${OPSMAN_USERNAME} -p ${OPSMAN_PASSWORD} credentials --product-name cf --credential-reference .uaa.admin_credentials --credential-field password)
 
 cf login -a https://login.${SYSTEM_DOMAIN} -u ${username} -p ${password}
 cf create-shared-domain ${TCP_DOMAIN} --router-group default-tcp
