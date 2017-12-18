d63ebb076a5a0686f6c84e1112ba5b0b825a5868
Chuck D'Antonio <c_dantonio@acm.org>
2017-12-18 12:48:05 -0500

    Deal with PKS wierdness

--- pipelines/install-pcf/tasks/upload-product-and-stemcell/task.sh
+++ pipelines/install-pcf/tasks/upload-product-and-stemcell/task.sh
@@ -96,4 +96,14 @@ fi
 
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
+if [ -z ${FILE_PATH} ] ; then
+  # deal with PKS team uploading zip files with a zip extension
+  FILE_PATH=`find ./pivnet-product -name *.zip | sort | head -1`
+fi
+
+if [ -z ${FILE_PATH} ] ; then
+  echo "Cannot identify product!"
+  exit 1
+fi
+
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
