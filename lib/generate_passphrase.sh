b92c803b423e95a558a4a8af102c0e11997b1121
Chuck D'Antonio <c_dantonio@acm.org>
2018-02-22 11:13:09 -0800

    Use standard Unix dictionary

--- lib/generate_passphrase.sh
+++ lib/generate_passphrase.sh
@@ -3,34 +3,11 @@
 
 generate_passphrase () {
   words=$1
-
-  cf_cli_github_raw_root="https://raw.githubusercontent.com/cloudfoundry/cli/master"
-  adjective_file="$work_dir/adjectives.txt"
-  noun_file="$work_dir/nouns.txt"
-  delimiter="-"
-
-  if [ ! -f "${adjective_file}" ] ; then
-    curl -qsLf -o "${adjective_file}" "${cf_cli_github_raw_root}/util/words/dict/adjectives.txt"
-  fi
-  adjective_lines=`wc -l ${adjective_file} | awk '{ print $1 }'`
-
-
-  if [ ! -f "${noun_file}" ] ; then
-    curl -qsLf -o "${noun_file}" "${cf_cli_github_raw_root}/util/words/dict/nouns.txt"
-  fi
-  noun_lines=`wc -l ${noun_file} | awk '{ print $1 }'`
-
   counter=0
+  delimiter="-"
   passphrase=""
   while [ $counter -lt $words ] ; do
-    if [ $((RANDOM%2)) -eq 0 ] ; then
-      file=$adjective_file
-      index=$((RANDOM*RANDOM%$adjective_lines+1));
-    else
-      file=$noun_file
-      index=$((RANDOM*RANDOM%$noun_lines+1));
-    fi
-    word=`sed -n "$index p" "${file}"`
+    word="$(gshuf -n1 /usr/share/dict/words)"
     passphrase="${passphrase}${word}"
     let counter=counter+1
     if [ $counter -lt $words ] ; then
@@ -38,5 +15,5 @@ generate_passphrase () {
     fi
   done
 
-  echo "${passphrase}"
+  echo "${passphrase}" | tr '[:upper:]' '[:lower:]'
 }
