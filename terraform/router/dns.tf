c0e313d7638a8a59a01060af052bc0c1ae294cc5
Chuck D'Antonio <c_dantonio@acm.org>
2018-08-19 12:12:39 -0400

    Continue with common network/vsphere split

--- terraform/router/dns.tf
+++ terraform/router/dns.tf
@@ -32,47 +32,6 @@ resource "google_dns_record_set" "router_alias" {
 
   rrdatas = [ "${local.router_fqdn}" ]
 }
-
-resource "google_dns_record_set" "vsphere" {
-  name    = "${local.vsphere_fqdn}"
-  type = "A"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.vsphere_ip}" ]
-}
-
-resource "google_dns_record_set" "vsphere_alias" {
-  name = "${local.vsphere_alias}"
-  type = "CNAME"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.vsphere_fqdn}" ]
-}
-
-resource "google_dns_record_set" "vcenter" {
-  name    = "${local.vcenter_fqdn}"
-  type = "A"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.vcenter_ip}" ]
-}
-
-resource "google_dns_record_set" "vcenter_alias" {
-  name = "${local.vcenter_alias}"
-  type = "CNAME"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.vcenter_fqdn}" ]
-}
-
 resource "google_dns_record_set" "outside" {
   name    = "${local.outside_fqdn}"
   type = "A"
---
+++ terraform/vsphere/dns.tf
@@ -0,0 +1,54 @@
+variable "dns_ttl" {
+  type = "string"
+}
+
+locals {
+  router_alias = "router.${var.domain}"
+  vsphere_alias = "esxi.${var.domain}"
+  vcenter_alias = "vcenter.${var.domain}"
+  outside_alias = "pigeon.${var.domain}"
+}
+
+data "google_dns_managed_zone" "homelab" {
+  name     = "crdant-net"
+}
+
+resource "google_dns_record_set" "vsphere" {
+  name    = "${local.vsphere_fqdn}"
+  type = "A"
+  ttl  = "${var.dns_ttl}"
+
+  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
+
+  rrdatas = [ "${local.vsphere_ip}" ]
+}
+
+resource "google_dns_record_set" "vsphere_alias" {
+  name = "${local.vsphere_alias}"
+  type = "CNAME"
+  ttl  = "${var.dns_ttl}"
+
+  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
+
+  rrdatas = [ "${local.vsphere_fqdn}" ]
+}
+
+resource "google_dns_record_set" "vcenter" {
+  name    = "${local.vcenter_fqdn}"
+  type = "A"
+  ttl  = "${var.dns_ttl}"
+
+  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
+
+  rrdatas = [ "${local.vcenter_ip}" ]
+}
+
+resource "google_dns_record_set" "vcenter_alias" {
+  name = "${local.vcenter_alias}"
+  type = "CNAME"
+  ttl  = "${var.dns_ttl}"
+
+  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
+
+  rrdatas = [ "${local.vcenter_fqdn}" ]
+}
