647b2e3d4e44f6f6320b3141c0046b6939a23b72
Chuck D'Antonio <c_dantonio@acm.org>
2018-08-28 21:42:09 -0400

    Tidy up unneeded and defaulted variables

--- lib/env.sh
+++ lib/env.sh
@@ -7,11 +7,7 @@ vsphere_user=root
 # important files and directories
 key_dir="${BASEDIR}/keys"
 work_dir="${BASEDIR}/work"
-etc_dir="${BASEDIR}/etc"
 state_dir="${BASEDIR}/state"
-pipeline_dir="${BASEDIR}/pipelines"
-ops_dir="${pipeline_dir}/ops"
-manifest_dir="${BASEDIR}/manifests"
 terraform_dir="${BASEDIR}/terraform"
 template_dir="${BASEDIR}/templates"
 
@@ -28,19 +24,13 @@ region=us-east1
 vcenter_iso_path="${work_dir}/VMware-VCSA-all-6.7.0-8546234.iso"
 vcenter_license=${VCENTER_LICENSE}
 
-# certificate configuration
-certbot_dir=/usr/local/etc/certbot
-ca_dir=${certbot_dir}/live
-ca_cert_file=${key_dir}/letsencrypt.pem
-
 # DNS
 domain=crdant.net
 subdomain=cloud.${domain}
-zone_id=Z267SNS3KA0BVO
 dns_ttl=60
 
 # NTP
-ntp_servers="0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org"
+ntp_servers=( "0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org" )
 
 # static hosts
 router_host="savasana"
@@ -60,22 +50,9 @@ bosh_director_role="BOSH Director"
 bosh_director_group="BoshDirectorServiceAccounts"
 
 infra_datastore=vsanDatastore
-vcenter_data_center=home-lab
-vcenter_cluster=primary-cluster
-vcenter_resource_pool_1=zone-1
-vcenter_resource_pool_2=zone-2
-vcenter_resource_pool_3=zone-3
-boostrap_switch=bootstrap-switch
-bootstrap_network=bootstrap-port-group
-pcf_switch=pcf-switch
-infrastructure_network=infra-port-group
-deployment_network=deployment-port-group
-services_network=services-port-group
-container_network=container-port-group
-balancer_internal_network=lb-internal-port-group
-balancer_external_network=lb-external-port-group
-dns_servers="172.20.0.0,1.1.1.1,8.8.8.8"
-dns_servers_array="[ 172.20.0.0,1.1.1.1,8.8.8.8 ]"
+
+dns_servers=( "1.1.1.1", "1.0.0.1", "8.8.8.8" )
+dns_servers_string="1.1.1.1,1.0.0.1,8.8.8.8"
 
 # PCF configuration
 om_datastore=vsanDatastore
@@ -84,6 +61,7 @@ om_ip_address=$(dig +short ${om_host_name}.${subdomain})
 director_host_name=director
 om_admin_user=arceus
 pcf_datastore=vsanDatastore
+
 ## Deployment domain names
 pcf_system_prefix=run
 pcf_apps_prefix=apps
