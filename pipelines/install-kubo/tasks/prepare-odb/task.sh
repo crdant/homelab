8507dea1bc45b337f4ef1ecd029b4981c5a1c2f5
Chuck D'Antonio <c_dantonio@acm.org>
2018-08-30 18:37:36 -0400

    Removed files from older iterations

--- pipelines/install-kubo/tasks/prepare-odb/task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-# workaround braindead naming in script
-cp odb-sdk/*.tgz odb-releases/on-demand-service-broker.tgz
-cp kubo-service-adapter/*.tgz odb-releases/kubo-service-adapter-release.tgz
-cp kubo-release/*.tgz odb-releases/kubo-release.tgz
-cp stemcell/*.tgz odb-releases
