e06504396f3ada78bf6bc165f7dcd8552d5586ad
Chuck D'Antonio <c_dantonio@acm.org>
2018-09-03 15:14:08 -0400

    Added outputs function. Sent push/pop output where it belongs.

--- lib/terraform.sh
+++ lib/terraform.sh
@@ -1,29 +1,37 @@
+
 init () {
   component=${1}
-  pushd "${terraform_dir}/${component}"
+  pushd "${terraform_dir}/${component}" > /dev/null
     terraform init \
       --backend-config=<(backend_config)
-  popd
+  popd > /dev/null
 }
 
 apply () {
   component=${1}
-  pushd "${terraform_dir}/${component}"
+  pushd "${terraform_dir}/${component}" > /dev/null
     terraform apply \
       --input=false \
       --auto-approve \
       --var-file=<(terraform_vars)
-  popd
+  popd > /dev/null
 }
 
 destroy () {
   component=${1}
-  pushd "${terraform_dir}/${component}"
+  pushd "${terraform_dir}/${component}" > /dev/null
     terraform destroy \
       --input=false \
       --auto-approve \
       --var-file=<(terraform_vars)
-  popd
+  popd > /dev/null
+}
+
+outputs () {
+  component=${1}
+  pushd "${terraform_dir}/${component}" > /dev/null
+    terraform output --json
+  popd > /dev/null
 }
 
 backend_config () {
