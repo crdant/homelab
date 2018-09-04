e880774d08a77c2d415d230db7694352f02a9a07
Chuck D'Antonio <c_dantonio@acm.org>
2018-09-04 11:54:20 -0400

    Initial prometheus implementation in the bootstrap area

--- terraform/bootstrap/prometheus.tf
+++ terraform/bootstrap/prometheus.tf
@@ -3,10 +3,40 @@ variable "prometheus_host" {
   default = "observe"
 }
 
+resource "random_pet" "prometheus_alert_manager_password" {
+  length = 4
+}
+
+resource "random_pet" "prometheus_alert_mesh_password" {
+  length = 4
+}
+
+resource "random_pet" "prometheus_password" {
+  length = 4
+}
+
+resource "random_pet" "prometheus_db_grafana_password" {
+  length = 4
+}
+
+resource "random_pet" "prometheus_grafana_password" {
+  length = 4
+}
+
+resource "random_pet" "prometheus_grafana_secret_key" {
+  length = 6
+}
+
 data "template_file" "prometheus_varfile" {
   template = "${file("${var.template_dir}/bootstrap/prometheus-vars.yml")}"
   vars {
-
+    # from the manifest
+    prometheus_alert_manager_password = "${random_pet.prometheus_alert_manager_password.id}"
+    prometheus_alert_mesh_password = "${random_pet.prometheus_alert_mesh_password.id}"
+    prometheus_password = "${random_pet.prometheus_password.id}"
+    prometheus_db_grafana_password = "${random_pet.prometheus_db_grafana_password.id}"
+    prometheus_grafana_password = "${random_pet.prometheus_grafana_password.id}"
+    prometheus_grafana_secret_key = "${random_pet.prometheus_grafana_secret_key.id}"
   }
 }
 
@@ -15,14 +45,10 @@ resource "local_file" "prometheus_varfile" {
   filename = "${var.work_dir}/bootstrap/prometheus-vars.yml"
 }
 
-data "template_file" "prometheus_manifest" {
-  template = "${file("${var.template_dir}/bootstrap/prometheus-vars.yml")}"
-  vars {
-
-  }
+resource "random_pet" "prometheus_password" {
+  length = 4
 }
 
-resource "local_file" "prometheus_manifest" {
-  content  = "${data.template_file.prometheus_manifest.rendered}"
-  filename = "${var.work_dir}/bootstrap/prometheus-vars.yml"
+resource "random_pet" "prometheus_grafana_password" {
+  length = 4
 }
