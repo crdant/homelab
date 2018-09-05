6d38030ff1df140c112693df149fd81dd020da42
Chuck D'Antonio <c_dantonio@acm.org>
2018-09-05 14:28:04 -0400

    Move Prometheus out of the bootstrap stack

--- terraform/bootstrap/certificates.tf
+++ terraform/bootstrap/certificates.tf
@@ -45,27 +45,3 @@ resource "local_file" "credhub_private_key" {
   content  = "${acme_certificate.credhub.private_key_pem}"
   filename = "${var.key_dir}/${acme_certificate.credhub.certificate_domain}/privkey.pem"
 }
-
-resource "acme_certificate" "prometheus" {
-  account_key_pem           = "${acme_registration.letsencrypt.account_key_pem}"
-  common_name               = "${local.prometheus_fqdn}"
-
-  dns_challenge {
-    provider = "gcloud"
-
-    config {
-      GCE_SERVICE_ACCOUNT_FILE = "${var.key_file}"
-      GCE_PROJECT  = "${var.project}"
-    }
-  }
-}
-
-resource "local_file" "prometheus_certificate" {
-  content  = "${acme_certificate.prometheus.certificate_pem}"
-  filename = "${var.key_dir}/${acme_certificate.prometheus.certificate_domain}/cert.pem"
-}
-
-resource "local_file" "prometheus_private_key" {
-  content  = "${acme_certificate.prometheus.private_key_pem}"
-  filename = "${var.key_dir}/${acme_certificate.prometheus.certificate_domain}/privkey.pem"
-}
--- terraform/bootstrap/dns.tf
+++ terraform/bootstrap/dns.tf
@@ -1,7 +1,6 @@
 locals {
   concourse_fqdn = "${var.concourse_host}.${var.domain}"
   credhub_fqdn = "${var.credhub_host}.${var.domain}"
-  prometheus_fqdn = "${var.prometheus_host}.${var.domain}"
 }
 
 data "google_dns_managed_zone" "homelab" {
@@ -26,14 +25,4 @@ resource "google_dns_record_set" "credhub" {
   managed_zone = "${data.google_dns_managed_zone.homelab.name}"
 
   rrdatas = [ "${local.credhub_ip}" ]
-}
-
-resource "google_dns_record_set" "prometheus" {
-  name    = "${local.prometheus_fqdn}."
-  type = "A"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.prometheus_ip}" ]
 }
