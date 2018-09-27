92f41ce24627da9414efbdef431fc3647dca8628
Chuck D'Antonio <c_dantonio@acm.org>
2018-09-27 09:46:03 -0400

    Move entries to load balancer to bootstrap

--- terraform/pave/dns.tf
+++ terraform/pave/dns.tf
@@ -1,23 +1,3 @@
 data "google_dns_managed_zone" "homelab" {
   name     = "homelab"
 }
-
-resource "google_dns_record_set" "bigip_management" {
-  name    = "${local.bigip_management_fqdn}."
-  type = "A"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.bigip_management_ip}" ]
-}
-
-resource "google_dns_record_set" "bigip_management_alias" {
-  name = "${local.bigip_management_alias}."
-  type = "CNAME"
-  ttl  = "${var.dns_ttl}"
-
-  managed_zone = "${data.google_dns_managed_zone.homelab.name}"
-
-  rrdatas = [ "${local.bigip_management_fqdn}." ]
-}
