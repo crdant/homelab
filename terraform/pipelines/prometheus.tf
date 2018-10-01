efd6a6a3df6b61822a1fd7525d61f6b8d9b658a3
Chuck D'Antonio <c_dantonio@acm.org>
2018-10-01 14:05:17 -0400

    First stab at Prometheus with pipelines

--- terraform/pipelines/prometheus.tf
+++ terraform/pipelines/prometheus.tf
@@ -12,19 +12,12 @@ locals {
   prometheus_secret_root = "${local.pcf_team_secret_root}/${var.prometheus_install_pipeline}"
 }
 
-resource "random_pet" "prometheus_alert_manager_password" {
-  length = 4
-}
-
-resource "random_pet" "prometheus_alert_mesh_password" {
-  length = 4
+resource "random_integer" "prometheus_availability_zone" {
+  min     = 0
+  max     = 2
 }
 
 resource "random_pet" "prometheus_password" {
-  length = 4
-}
-
-resource "random_pet" "prometheus_db_grafana_password" {
   length = 4
 }
 
@@ -32,27 +25,54 @@ resource "random_pet" "prometheus_grafana_password" {
   length = 4
 }
 
-resource "random_pet" "prometheus_grafana_secret_key" {
+resource "random_pet" "prometheus_alert_manager_password" {
+  length = 4
+}
+
+resource "random_pet" "firehose_exporter_secret" {
   length = 6
+}
+
+resource "random_pet" "cf_exporter_secret" {
+  length = 6
+}
+
+resource "random_pet" "bosh_exporter_secret" {
+  length = 6
+}
+
+data "template_file" "prometheus_params" {
+  template = "${file("${var.template_dir}/pipelines/prometheus/params.yml")}"
+  vars {
+    opsman_domain = "${local.opsman_fqdn}"
+    network = "${data.terraform_remote_state.pave.deployment_network}"
+    availability_zone_1 = "${var.availability_zones[random_integer.prometheus_availability_zone.result]}"
+  }
+}
+
+resource "local_file" "prometheus_params" {
+  content  = "${data.template_file.prometheus_params.rendered}"
+  filename = "${var.work_dir}/pipelines/prometheus/params.yml"
 }
 
 data "template_file" "prometheus_secrets" {
   template = "${file("${var.template_dir}/pipelines/prometheus/secrets.yml")}"
   vars {
-    # from the manifest
     prometheus_secret_root = "${local.prometheus_secret_root}"
-    prometheus_alert_manager_password = "${random_pet.prometheus_alert_manager_password.id}"
-    prometheus_alert_mesh_password = "${random_pet.prometheus_alert_mesh_password.id}"
+
     prometheus_password = "${random_pet.prometheus_password.id}"
-    prometheus_db_grafana_password = "${random_pet.prometheus_db_grafana_password.id}"
     prometheus_grafana_password = "${random_pet.prometheus_grafana_password.id}"
-    prometheus_grafana_secret_key = "${random_pet.prometheus_grafana_secret_key.id}"
+    prometheus_alert_manager_password = "${random_pet.prometheus_alert_manager_password.id}"
+
+    uaa_clients_firehose_exporter_secret = "${random_pet.firehose_exporter_secret.id}"
+    uaa_clients_cf_exporter_secret = "${random_pet.cf_exporter_secret.id}"
+    uaa_clients_bosh_exporter_secret = "${random_pet.bosh_exporter_secret.id}"
   }
 }
 
 resource "local_file" "prometheus_secrets" {
   content  = "${data.template_file.prometheus_secrets.rendered}"
-  filename = "${var.key_dir}/pipelines/prometheus-secrets.yml"
+  filename = "${var.key_dir}/pipelines/prometheus/secrets.yml"
 
   provisioner "local-exec" {
     command =<<COMMAND
@@ -69,5 +89,10 @@ output "prometheus_password" {
 
 output "prometheus_grafana_password" {
   value = "${random_pet.prometheus_grafana_password.id}"
+  sensitive = true
+}
+
+output "prometheus_alert_manager_password" {
+  value = "${random_pet.prometheus_alert_manager_password.id}"
   sensitive = true
 }
