68a283de62678125cb1ea4b3ab1dc683ae6d9079
Chuck D'Antonio <c_dantonio@acm.org>
2018-11-27 18:11:09 -0500

    Need a NAT address for the apps side, too

--- terraform/modules/cluster/cluster.tf
+++ terraform/modules/cluster/cluster.tf
@@ -15,5 +15,6 @@ variable "cluster_port" {
 locals {
   cluster_bigip_ip = "${cidrhost(local.balancer_external_cidr,var.cluster_index)}"
   cluster_bigip_internal_ip = "${cidrhost(local.balancer_internal_cidr,var.cluster_index)}"
+  apps_bigip_internal_ip = "${cidrhost(local.balancer_internal_cidr,var.cluster_index + 1)}"
   cluster_fqdn = "${var.cluster}.${data.terraform_remote_state.pipelines.pks_subdomain}"
 }
